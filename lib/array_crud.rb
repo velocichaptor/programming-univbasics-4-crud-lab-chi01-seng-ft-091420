e178a97921a6bfd8a0df761596066c723a13a95a
Jake Chappelle <jake.m.chappelle@gmail.com>
2020-08-16 20:51:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -42,5 +42,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-
+   array = ["arrays!", "luigi", "wario",  "waluigi"]
+   array[index_number]
 end
