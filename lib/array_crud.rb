4e3971bacf379004c4d84f8ba672548d76c25af2
Jake Chappelle <jake.m.chappelle@gmail.com>
2020-08-16 20:46:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-   array = ["arrays!", "luigi", "wario",  "waluigi"]
+   array = ["wow", "luigi", "wario",  "waluigi"]
    array.shift
 end
 
